06ac6769bd7450fb42d9cf88db4c14748a179e2c
fmarga <marga.fmartins@gmail.com>
2022-07-14 22:24:55 -0300

    refactor: changes get/tests method at server.rb

--- server.rb
+++ server.rb
@@ -5,10 +5,21 @@ require './medical_record'
 
 get '/tests' do
   conn = PG.connect(host: 'postgres', user: 'postgres', password: 'pass')
-  conn
-    .exec('SELECT * FROM records')
-    .map { |row| row }
-    .to_json
+  # conn
+  #   .exec('SELECT * FROM records')
+  #   .map { |row| row }
+  #   .to_json
+
+  results = conn.exec('SELECT * FROM records')
+
+  columns = results.fields
+
+  results.map do |result|
+    result.each_with_object({}).with_index do |(cell, acc), idx|
+      column = columns[idx]
+      acc[column] = cell[1]
+    end
+  end.to_json
 end
 
 Rack::Handler::Puma.run(
--- tests/setup_database_test.rb
+++ tests/setup_database_test.rb
@@ -7,9 +7,9 @@ class SetupDatabaseTest < Test::Unit::TestCase
     csv = CSV.read('./tests/test_data.csv', headers: true, col_sep: ';').map(&:fields)
     conn = PG.connect(host: 'postgres', user: 'postgres', password: 'pass')
 
-    SetupDatabase.new('./tests/test_data.csv', 'postgres').from_csv
+    SetupDatabase.new('./tests/test_data.csv').from_csv
     db = conn.exec('SELECT * FROM records').map(&:values)
 
-    assert_equal(db, csv)
+    assert_equal db, csv
   end
 end
