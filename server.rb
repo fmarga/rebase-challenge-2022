120feaf0f9088e53aedcfca3012b22c4bfd0ac91
fmarga <marga.fmartins@gmail.com>
2022-07-22 17:39:15 -0300

    refactor: changes server file

--- server.rb
+++ server.rb
@@ -1,34 +1,36 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'rack/handler/puma'
 require 'pg'
 require './data_worker'
 require './setup_database'
 require './result'
 
-get '/' do
-  'Hello World'
-end
+class Application < Sinatra::Base
+  set :bind, '0.0.0.0'
+  set :port, 3000
 
-get '/tests' do
-  Result.select_tests.to_json
-end
-
-get '/tests/:token' do
-  token = params[:token]
-  Result.find_token(token).to_json
-end
-
-post '/import' do
-  begin
-    DataWorker.perform_async(request.body.read)
-    201
-  rescue
-    404
+  get '/' do
+    'Hello World'
   end
-end
 
-Rack::Handler::Puma.run(
-  Sinatra::Application,
-  Port: 3000,
-  Host: '0.0.0.0'
-)
+  get '/tests' do
+    Result.select_tests.to_json
+  end
+
+  get '/tests/:token' do
+    token = params[:token]
+    Result.find_token(token).to_json
+  end
+
+  post '/import' do
+    begin
+      DataWorker.perform_async(request.body.read)
+      201
+    rescue
+      404
+    end
+  end
+
+  run! if app_file == $0
+end
